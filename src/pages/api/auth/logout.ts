2660963948dee6d14f215edb7de3de9a68de118b
04AR <anjalraisam04@gmail.com>
2026-05-03 15:48:46 +0530

    fixing logout

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,11 +1,14 @@
 import type { APIRoute } from 'astro';
 
-export const POST: APIRoute = async ({ cookies }) => {
-  cookies.delete('token', { 
-    path: '/',
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
-    httpOnly: true
+export const POST: APIRoute = async () => {
+  const secure = process.env.NODE_ENV === 'production' ? 'Secure;' : '';
+  const cookieString = `token=deleted; Path=/; Max-Age=0; HttpOnly; SameSite=Lax; ${secure}`;
+
+  return new Response(JSON.stringify({ success: true }), { 
+    status: 200,
+    headers: {
+      'Set-Cookie': cookieString,
+      'Content-Type': 'application/json'
+    }
   });
-  return new Response(JSON.stringify({ success: true }), { status: 200 });
 }
